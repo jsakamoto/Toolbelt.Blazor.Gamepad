66817d5d097f1aab2a2c48e1455f9db07978e0af
jsakamoto <junichi.jp@gmail.com>
2021-09-14 21:00:36 +0900

    Fix: loading the helper JavaScript code was failed on .NET Core 3.1

--- Toolbelt.Blazor.Gamepad/script.js
+++ Toolbelt.Blazor.Gamepad/script.js
@@ -8,7 +8,7 @@ var Toolbelt;
             var _a, _b;
             const searchParam = ((_b = (_a = document.currentScript) === null || _a === void 0 ? void 0 : _a.getAttribute('src')) === null || _b === void 0 ? void 0 : _b.split('?')[1]) || '';
             var r = import('./script.module.min.js?' + searchParam).then(m => {
-                Object.assign(Gamepad, Toolbelt.Blazor.Gamepad);
+                Object.assign(Gamepad, m.Toolbelt.Blazor.Gamepad);
             });
             function ready() { return r; }
             Gamepad.ready = ready;
--- Toolbelt.Blazor.Gamepad/script.ts
+++ Toolbelt.Blazor.Gamepad/script.ts
@@ -1,7 +1,7 @@
 ﻿namespace Toolbelt.Blazor.Gamepad {
     const searchParam = document.currentScript?.getAttribute('src')?.split('?')[1] || '';
     var r = import('./script.module.min.js?' + searchParam).then(m => {
-        Object.assign(Gamepad, Toolbelt.Blazor.Gamepad);
+        Object.assign(Gamepad, m.Toolbelt.Blazor.Gamepad);
     });
     export function ready(): Promise<void> { return r; }
 }
