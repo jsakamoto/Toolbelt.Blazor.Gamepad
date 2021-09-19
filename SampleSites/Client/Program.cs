51d1eff4840a8bbf8333b9d9d6ca95719b63683b
jsakamoto <junichi.jp@gmail.com>
2021-09-19 22:05:30 +0900

    Improve sample sites

--- SampleSites/Client/Program.cs
+++ SampleSites/Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using SampleSite.Components;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
@@ -11,12 +12,19 @@ namespace SampleSite.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            builder.Services.AddGamepadList(options =>
+            
+            ConfigureServices(builder.Services);
+
+            await builder.Build().RunAsync();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHeadElementHelper();
+            services.AddGamepadList(options =>
             {
                 //options.DisableClientScriptAutoInjection = true;
             });
-
-            await builder.Build().RunAsync();
         }
     }
 }
