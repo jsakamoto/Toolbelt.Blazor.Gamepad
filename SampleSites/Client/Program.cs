54cdfcd1c8abd992d7ec67d63b77247d382635dc
jsakamoto <junichi.jp@gmail.com>
2023-12-14 07:47:02 +0900

    Fix: the Blazor Wasm sample project could not publish

--- SampleSites/Client/Program.cs
+++ SampleSites/Client/Program.cs
@@ -1,17 +1,22 @@
-﻿using SampleSite.Components;
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SampleSite.Components;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddHeadElementHelper();
-builder.Services.AddGamepadList(options =>
-{
-    //options.DisableClientScriptAutoInjection = true;
-});
+ConfigureServices(builder.Services, builder.HostEnvironment);
 
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, IWebAssemblyHostEnvironment hostEnv)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(hostEnv.BaseAddress) });
+    services.AddHeadElementHelper();
+    services.AddGamepadList(options =>
+    {
+        //options.DisableClientScriptAutoInjection = true;
+    });
+}
